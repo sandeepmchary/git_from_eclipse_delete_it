c5eb9818d1a8106d8f238f231eaca64e1c414948
sandeepmchary <awssandeepchary@gmail.com>
2022-05-20 17:58:24 +0530

    added

---
+++ GitHub-from-eclipse/src/main/java/com/javatechie/GitHub_from_eclipse/App.java
@@ -0,0 +1,14 @@
+package com.javatechie.GitHub_from_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+         System.out.println( "Changes done by John" );
+        System.out.println( "Hello World!"+"First change from Java Techie" );
+    }
+}
